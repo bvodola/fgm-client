bfadac26cab1d9f3d709528894b6f4639ea98b54
Brunno Vodola <bvodola@gmail.com>
2020-02-27 16:47:19 -0300

    Fixing users and receipts forEach

--- src/screens/Reports/index.js
+++ src/screens/Reports/index.js
@@ -61,71 +61,75 @@ class Reports extends React.Component {
     const { users, dateRange, filterByDate, splitByCompanyType } = this.state;
     let tableData = [];
 
-    users.forEach(user => {
-      user.receipts.forEach(receipt => {
-        // Loop each receipt
+    Array.isArray(users) &&
+      users.forEach(user => {
+        Array.isArray(user.receipts) &&
+          user.receipts.forEach(receipt => {
+            // Loop each receipt
 
-        // The created date is either the one from the receipt or the user
-        const createdDate = new Date(Number(receipt.created || user.created));
+            // The created date is either the one from the receipt or the user
+            const createdDate = new Date(
+              Number(receipt.created || user.created)
+            );
 
-        // IF condition that filters chart data by the dates chosen in the filter
-        if (
-          !filterByDate ||
-          (Array.isArray(dateRange) &&
-            createdDate >= dateRange[0] &&
-            createdDate <= dateRange[1])
-        ) {
-          // Variable setting
-          let appendToData = true;
-          const startOfDay = new Date(
-            `${createdDate.getFullYear()}-${createdDate.getMonth() +
-              1}-${createdDate.getDate()}`
-          );
+            // IF condition that filters chart data by the dates chosen in the filter
+            if (
+              !filterByDate ||
+              (Array.isArray(dateRange) &&
+                createdDate >= dateRange[0] &&
+                createdDate <= dateRange[1])
+            ) {
+              // Variable setting
+              let appendToData = true;
+              const startOfDay = new Date(
+                `${createdDate.getFullYear()}-${createdDate.getMonth() +
+                  1}-${createdDate.getDate()}`
+              );
 
-          console.log("receipt", receipt);
-          // First, we check if the current receipt date is already present on tableData
-          tableData = tableData.map(e => {
-            if (e[0].getTime() === startOfDay.getTime()) {
-              appendToData = false;
-              if (splitByCompanyType) {
-                if (
-                  receipt.dental_name
-                    .toLowerCase()
-                    .trim()
-                    .search("fgm") >= 0
-                ) {
-                  e[1]++;
-                } else {
-                  e[2]++;
+              console.log("receipt", receipt);
+              // First, we check if the current receipt date is already present on tableData
+              tableData = tableData.map(e => {
+                if (e[0].getTime() === startOfDay.getTime()) {
+                  appendToData = false;
+                  if (splitByCompanyType) {
+                    if (
+                      receipt.dental_name
+                        .toLowerCase()
+                        .trim()
+                        .search("fgm") >= 0
+                    ) {
+                      e[1]++;
+                    } else {
+                      e[2]++;
+                    }
+                  } else {
+                    e[1]++;
+                  }
                 }
-              } else {
-                e[1]++;
-              }
-            }
-            return e;
-          });
+                return e;
+              });
 
-          // If not, we append it and set the initial numberOfReceipts value
-          if (appendToData) {
-            if (splitByCompanyType) {
-              if (
-                receipt.dental_name
-                  .toLowerCase()
-                  .trim()
-                  .search("fgm") >= 0
-              ) {
-                e[1]++;
-                tableData.push([startOfDay, 1, 0]);
-              } else {
-                tableData.push([startOfDay, 0, 1]);
+              // If not, we append it and set the initial numberOfReceipts value
+              if (appendToData) {
+                if (splitByCompanyType) {
+                  if (
+                    receipt.dental_name
+                      .toLowerCase()
+                      .trim()
+                      .search("fgm") >= 0
+                  ) {
+                    e[1]++;
+                    tableData.push([startOfDay, 1, 0]);
+                  } else {
+                    tableData.push([startOfDay, 0, 1]);
+                  }
+                } else {
+                  tableData.push([startOfDay, 1]);
+                }
               }
-            } else {
-              tableData.push([startOfDay, 1]);
             }
-          }
-        }
+          });
       });
-    });
 
     tableData = splitByCompanyType
       ? [["Data", "FGM", "Dentais"], ...tableData]
